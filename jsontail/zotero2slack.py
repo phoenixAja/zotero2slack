626cf9e6c0723f7142280b900dc409e6aa6c5ac8
James Webber <james@SBlab.ucsf.edu>
2015-10-23 16:03:42 -0700

    fixed bug with missing names

--- jsontail/zotero2slack.py
+++ jsontail/zotero2slack.py
@@ -7,9 +7,9 @@ import requests
 # this formats a JSON entry and makes a nice Slack message with
 # the person who added it, the paper title and journal, and a link
 def format_json(entry):
+    name = entry['meta']['createdByUser']['name'] or entry['meta']['createdByUser']['username']
     return '{creator} added <{url}|{title}> - {journalAbbreviation}'.format(
-        creator=entry['meta']['createdByUser'].get('name', entry['meta']['createdByUser']['username']),
-        **entry['data'])
+        creator=name, **entry['data'])
 
 
 # this is a hook for taking the new entries and sending them to a Slack app
